8cd7bb6f8ad57856854af72414d5d43170469949
Appala Naidu Gadu <46554685+arrayappy@users.noreply.github.com>
2020-05-13 22:59:58 +0530

    Add files via upload

---
+++ LeetCode/CelsiusToFahrenheit.cpp
@@ -0,0 +1,24 @@
+/*
+ * Created : 2020-03-01 09:41:09
+ * Author : Appala Naidu Gadu
+ * Handle : arrayappy
+ * Email : arrayappy@gmail.com
+ * StopStalk : https://www.stopstalk.com/user/profile/arrayappy
+ * Motto : Keep Calm and Code On.
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{   ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int c,f;
+		cin>>c;
+		f=(c*1.8)+32;
+		cout<<f<<endl;
+	}
+	return 0;
+}
+
